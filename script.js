ac2bc05c946351b718449dedf4869072891f96cc
Hojimurod <hojimurod675@gmail.com>
2024-09-18 21:45:29 +0500

    create html

---
+++ script.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ismingizni kiriting");
+
+let hozirgi = prompt("Hozirgi yilni kiriting");
+let tugilgan = prompt("Tug'ilgan yilingizni kiriting");
+let = alert("Marhamat consoleni oching ☺️")
+
+function age(hozirgi, tugilgan) {
+    return hozirgi - tugilgan;
+}
+
+let yosh = age(hozirgi, tugilgan);
+console.log("Ismingiz: " + ism + ", sizning yoshingiz: " + yosh);
